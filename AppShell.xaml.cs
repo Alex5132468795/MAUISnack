d0219a03e1ed205c287493d61da614efeadb6911
ilyaDavidov <ilya.davidov63@mail.ru>
2023-02-06 19:33:19 +0200

    f

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiApp18;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace MauiApp18;
+using CommunityToolkit;
+using CommunityToolkit.Maui.Alerts;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+	private void Button_Clicked(object sender, EventArgs e)
+	{
+		CancellationToken c = new CancellationToken();
+		Snackbar snackbar = new Snackbar(); //{Text.color = Color.FromRgb(0, 255, 0) };
+		snackbar = (Snackbar) Snackbar.Make("OWO", async () => await DisplayAlert("Welcom", "be-bei see you", "Okey"), "UWU", TimeSpan.FromSeconds(10));
+		snackbar.Show();
+	}
+}
+
